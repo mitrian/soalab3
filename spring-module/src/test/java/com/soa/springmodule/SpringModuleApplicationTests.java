68a4d8ec6ac5056abb8d52b4fbc364b5abbecf7b
mitrian <onlyvideo01@gmail.com>
2025-12-19 16:21:56 +0300

    chore(backend): spring multi-module structure

---
+++ spring-module/src/test/java/com/soa/springmodule/SpringModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soa.springmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
